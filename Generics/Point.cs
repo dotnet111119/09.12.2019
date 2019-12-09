83ad7ddb96a31adf72dc6f456726c5c4391689dc
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-09 19:17:21 +0200

    Add files via upload

---
+++ Generics/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0912
+{
+    public class Point<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public Point(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X : {X} Y: {Y}";
+        }
+    }
+}
