c7d87096870eadf2f4d5d27cf039eb7fae887ad4
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-09 19:10:43 +0200

    Add files via upload

---
+++ Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0912
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add(1);
+            arr.Add(3);
+            //arr.Add("hello");
+
+            int sum = 0;
+            foreach (int item in arr)
+            {
+                int number = (int)item;
+                sum += number;
+            }
+            Console.WriteLine(sum);
+
+            //List<>
+            Secret<int> s = new Secret<int>(1);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Generics/Secret.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0912
+{
+    public class Secret<T>
+    {
+        public T Data { get; set; }
+        public Secret(T data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return $"{Data}";
+        }
+
+    }
+}
